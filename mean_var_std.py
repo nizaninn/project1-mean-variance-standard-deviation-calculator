24c4f95b4847e5cbb1b24f89e70b49dfde2cbc6a
Nicole Zanin <nicole.silva@sou.inteli.edu.br>
2025-08-19 13:13:59 -0300

    feat: project1

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,19 @@
 import numpy as np
 
-def calculate(list):
+def calculate(lst):
+ if len(lst) !=9:
+  raise ValueError("The list must contain 9 items")
 
+ arr=np.array(lst).reshape(3, 3)
 
+ def get_stats(func):
+        return [func(arr, axis=0).tolist(), func(arr, axis=1).tolist(), func(arr).item()]
 
-
-    return calculations
+ return {
+ 'mean': get_stats(np.mean),
+ 'variance': get_stats(np.var),
+ 'standard deviation': get_stats(np.std),
+ 'max': get_stats(np.max),
+ 'min': get_stats(np.min),
+ 'sum': get_stats(np.sum)
+}
